e9301c35d155e19830149d3d193c18165af3661e
Fuji <senfu26@gmail.com>
2024-05-14 20:10:47 +0700

    Push Ulang

--- gangguanGejala.js
+++ gangguanGejala.js
@@ -2,6 +2,27 @@ const nama_penyakit = [
   { nama: "CONTRACT ULCERS", gejala: [1] },
   { nama: "ABAES PARAFARINGEAL", gejala: [2] },
   { nama: "ABAES PERITONAILER", gejala: [3] },
+  { nama: "BAROTITIS MEDIA", gejala: [4] },
+  { nama: "DEVIASI SEPTUM", gejala: [5] },
+  { nama: "FARINGITIS", gejala: [6] },
+  { nama: "KANKER LARING", gejala: [7] },
+  { nama: "KANKER LEHER DAN KEPALA", gejala: [8] },
+  { nama: "KANKER LEHER METASTATIK", gejala: [9] },
+  { nama: "KANKER NASOFARING", gejala: [10] },
+  { nama: "KANKER TONSIL", gejala: [11] },
+  { nama: "LARINGITIS", gejala: [12] },
+  { nama: "NEURONITIS VESTIBULARIS", gejala: [13] },
+  { nama: "OSTEOSKLEROSIS", gejala: [14] },
+  { nama: "OTITIS MEDIA AKUT", gejala: [15] },
+  { nama: "MENIERE", gejala: [16] },
+  { nama: "TONSILITIS", gejala: [17] },
+  { nama: "TUMOR SYARAF PENDENGARAN", gejala: [18] },
+  { nama: "VERTIGO POSTULAR", gejala: [19] },
+  { nama: "SINUSITIS MAKSILARIS", gejala: [20] },
+  { nama: "SINUSITIS FRONTALIS", gejala: [21] },
+  { nama: "SINUSITIS ETMOIDALIS", gejala: [22] },
+  { nama: "SINUSITIS SFENOIDALIS", gejala: [23] },
+  { nama: "PERUT", gejala: [24] },
   // Tambahkan data nama_penyakit lainnya sesuai dengan data yang diberikan
 ];
 
@@ -19,20 +40,18 @@ function diagnosa() {
   const hasilDiagnosaDiv = document.getElementById("hasilDiagnosa");
   const hasilText = document.getElementById("hasilText");
   if (hasilDiagnosa.length > 0) {
-    hasilText.textContent =
-      "Berdasarkan gejala yang Anda alami, kemungkinan penyakit yang Anda derita adalah: " +
-      hasilDiagnosa.join(", ");
-  } else {
+    hasilText.innerHTML =
+      "Berdasarkan gejala yang Anda alami, kemungkinan penyakit yang Anda derita adalah: <u>" +
+      hasilDiagnosa.join("<span class='highlighted'> | </span>") +
+      "</u>";
+  }
+   else {
     hasilText.textContent =
       "Maaf, berdasarkan gejala yang Anda alami, tidak dapat didiagnosa penyakit THT yang spesifik.";
   }
   hasilDiagnosaDiv.style.display = "block";
 }
-function toggleCheckbox(checkbox) {
-  const checkboxes = document.querySelectorAll('input[name="gejala"]');
-  checkboxes.forEach((cb) => {
-    if (cb !== checkbox) {
-      cb.checked = false;
-    }
-  });
+function toggleDivCheckbox(div) {
+  var checkbox = div.querySelector('input[type="checkbox"]');
+  checkbox.checked = !checkbox.checked;
 }
--- pilihPenyakit.js
+++ pilihPenyakit.js
@@ -1,15 +1,51 @@
   // Data penyakit dan gejala
   const penyakitGejala = {
-    "CONTRACT ULCERS": ["nyeri saat bicara atau menelan", "suara serak"],
-    "ABAES PARAFARINGEAL": ["gejala1", "gejala2"],
-    // Tambahkan data penyakit dan gejala lainnya 
-};
+      "CONTRACT ULCERS": ["nyeri saat bicara atau menelan", "suara serak"],
+      "ABAES PARAFARINGEAL": ["nyeri saat bicara atau menelan", "leher bengkak"],
+      "ABAES PERITONAILER": ["demam", "sakit kepala","nyeri tenggorokan","pembengkakan kelenjar getah bening","suara serak","air liur menetes"],
+      "BAROTITIS MEDIA": ["sakit kepala", "nyeri telinga"],
+      "DEVIASI SEPTUM": ["demam", "hidung tersumbat", "nyeri telinga", "pendarahan hidung", "infeksi sinus", "nyeri wajah"],
+      "FARINGITIS": ["demam", "nyeri saat bicara atau menelan", "nyeri tenggorokan", "nyeri leher", "pembengkakan kelenjar getah bening"],
+      "KANKER LARING": ["nyeri saat bicara atau menelan", "batuk", "nyeri tenggorokan", "nyeri leher", "suara serak", "berat badan turun", "bunyi nafas abnormal"],
+      "KANKER LEHER DAN KEPALA": ["nyeri saat bicara atau menelan", "ada benjolan di leher", "pendarahan hidung", "ada yang tumbuh di mulut", "perubahan warna kulit", "perubahan suara"],
+      "KANKER LEHER METASTATIK": ["ada benjolan di leher"],
+      "KANKER NASOFARING": ["hidung tersumbat", "pendarahan hidung"],
+      "KANKER TONSIL": ["nyeri tenggorokan", "ada benjolan di leher"],
+      "LARINGITIS": ["demam", "nyeri saat bicara atau menelan", "pembengkakan kelenjar getah bening", "leher bengkak", "tenggorokan gatal"],
+      "NEURONITIS VESTIBULARIS": ["mual dan muntah", "bola mata bergerak tanpa sadar"],
+      "OSTEOSKLEROSIS": ["tuli", "telinga berdenging"],
+      "OTITIS MEDIA AKUT": ["demam", "nyeri telinga", "mual dan muntah", "radang gendang telinga"],
+      "MENIERE": ["nyeri telinga", "mual dan muntah", "serangan vertigo", "telinga terasa penuh"],
+      "TONSILITIS": ["demam", "sakit kepala", "nyeri saat bicara atau menelan", "batuk", "nyeri tenggorokan", "mual dan muntah"],
+      "TUMOR SYARAF PENDENGARAN": ["sakit kepala", "tuli", "tubuh tak seimbang"],
+      "VERTIGO POSTULAR": ["bola mata bergerak tanpa sadar"],
+      "SINUSITIS MAKSILARIS": ["demam", "sakit kepala", "batuk", "hidung tersumbat", "hidung meler", "letih dan lesu", "selaput lendir merah dan bengkak", "nyeri pipi dibawah mata", "sakit gigi"],
+      "SINUSITIS FRONTALIS": ["demam", "sakit kepala", "batuk", "hidung tersumbat", "hidung meler", "letih dan lesu", "selaput lendir merah dan bengkak", "dahi sakit"],
+      "SINUSITIS ETMOIDALIS": ["demam", "sakit kepala", "batuk", "hidung tersumbat", "hidung meler", "letih dan lesu", "selaput lendir merah dan bengkak", "dahi sakit", "nyeri antara mata", "nyeri pinggir hidung"],
+      "SINUSITIS SFENOIDALIS": ["demam", "sakit kepala", "batuk", "hidung tersumbat", "nyeri telinga", "hidung meler", "letih dan lesu", "selaput lendir merah dan bengkak", "ada benjolan di leher"],
+      "PERUT": ["demam", "sakit kepala", "nyeri saat bicara atau menelan", "batuk"],
+}
+  
 
-// Fungsi untuk menampilkan gejala yang sesuai dengan nama penyakit yang dipilih
+
+
 function tampilkanGejala() {
-    const penyakitTerpilih = document.getElementById("penyakit").value;
-    const gejalaDiv = document.getElementById("gejala");
-    const gejalaText = document.getElementById("gejalaText");
-    gejalaText.textContent = penyakitGejala[penyakitTerpilih] ? penyakitGejala[penyakitTerpilih].join(", ") : "";
-    gejalaDiv.style.display = penyakitGejala[penyakitTerpilih] ? "block" : "none";
+  const select = document.getElementById("penyakit");
+  const penyakit = select.value;
+  const gejalaContainer = document.getElementById("gejala");
+  const gejalaList = document.getElementById("gejalaText");
+  const gejalaCount = document.getElementById("gejalaCount");
+
+  const gejala = penyakitGejala[penyakit] || [];
+  const jumlahGejala = gejala.length;
+
+  gejalaList.innerHTML = ""; 
+  gejala.forEach((item) => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      gejalaList.appendChild(li);
+  });
+
+  gejalaCount.textContent = `Terdapat ${jumlahGejala} Gejala yang Dialami:`;
+  gejalaContainer.style.display = jumlahGejala > 0 ? "block" : "none";
 }
